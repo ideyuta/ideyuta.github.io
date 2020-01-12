c84cdeb537a8adf3d43e1bcefebf4c23446c82b1
ideyuta <ide.yuta@gmail.com>
2020-01-12 15:15:59 +0900

    Add sofar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Helmet from 'react-helmet';
 import styled from 'styled-components';
 import Footer from '../components/Footer';
@@ -10,6 +11,10 @@ import Spacer from '../components/Spacer';
 import Reaction from '../components/Reaction';
 import { colors } from '../assets';
 
+const A = styled(Link)`
+  display: block;
+  padding: 0.3em 0;
+`;
 const Date = styled.p`
   color: ${colors.gray500};
 `;
@@ -70,6 +75,14 @@ export default function Index() {
           />
 
           <p>略歴 88年静岡県生まれ。多摩美術大学大学院 美術研究科 デザイン専攻 情報デザイン研究領域 経験デザイン修了。美術修士。4歳と1歳の娘がいます。高校で電子工学、大学でメディアアート、大学院でインタラクションデザインをやっていました。フリーのデザイナーを経て、2013年8月に金融・決済・ITを軸とするベンチャーであるカンムへ入社。CLOの立ち上げからデザインと開発を行い、2016年9月にバンドルカードをリリースしました。2017年3月、カンムを退職し、フリークアウト・ホールディングス子会社のニューライトへ。代表としてSMB向け業務改善事業のあれこれをやっていましたが撤退。2018年11月、heyにてsoiという事業を立ち上げました。最近は社運がかかっていると噂のCoineyとSTORESをつなぐプロジェクトをやっています。</p>
+      </Section>
+
+      <Section>
+        <SectionHeader>
+          SOFAR
+        </SectionHeader>
+        <A to="one_fine_day">ある晴れた日のこと</A>
+        <A to="2020/01">2020/01</A>
       </Section>
 
       <Footer />
