1f202b57ef99ef45e86bba141dc0d2f2fe9b15b4
ideyuta <ide.yuta@gmail.com>
2020-01-12 22:47:06 +0900

    Fix Footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { colors } from '../assets';
 
 const Wrapper = styled.div`
+  background: ${colors.gray20};
   box-sizing: border-box;
   padding: 32px 24px;
   width: 100vw;
